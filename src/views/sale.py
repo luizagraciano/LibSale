c9e513d3a1c0a0c0369cf06a0e973a48b664af09
Luiza Graciano <luizagraciano.m@gmail.com>
2024-09-09 13:30:47 -0300

    atualiza modulo e pagina de venda

--- src/views/sale.py
+++ src/views/sale.py
@@ -8,4 +8,24 @@ bp = Blueprint('sale', __name__, url_prefix='/sale')
 
 @bp.route('/')
 def new_sale():
-    return render_template('pos/sale.html')
+
+    db = get_db()
+
+    current_cash = db.execute(
+        'SELECT * FROM cash_register WHERE id = (SELECT MAX(id) FROM cash_register)'
+    ).fetchone()
+
+    status = current_cash['status']
+    seller = session['user_id']
+
+    if status == 'Aberto':
+        db.execute(
+            'INSERT INTO sale (seller_id) VALUES(?)', (seller,)
+        )
+        db.commit()
+
+        return render_template('pos/sale.html')
+    else:
+        return redirect(url_for('dashboard.welcome'))
+    
+
