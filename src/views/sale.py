7dfb87a327c3d2b70c5bd157e0d25b618d1b5e33
Luiza Graciano <luizagraciano.m@gmail.com>
2024-09-09 19:36:54 -0300

    atualiza modulo de venda

--- src/views/sale.py
+++ src/views/sale.py
@@ -33,14 +33,34 @@ def new_sale():
         return redirect(url_for('sale.update_sale', id = id))
     else:
         return redirect(url_for('dashboard.welcome'))
-    
 
-@bp.route('/<int:id>/')
+
+def get_product(id):
+    db = get_db()
+    product = db.execute(
+        'SELECT * FROM product WHERE id = ?', (id,)
+    ).fetchone()
+
+    return product
+
+@bp.route('/<int:id>/', methods=('GET', 'POST'))
 def update_sale(id):
+
+    if request.method == 'POST':
+        product_id = request.form['product-id']
+
+        product = get_product(product_id)
+
+        db = get_db()
+        db.execute(
+            'INSERT INTO sale_item (product_id, product_price, quantity, subtotal, sale_id) VALUES (?, ?, ?, ?, ?)',
+            (product['id'], product['price'], '1', product['price'], id)
+        )
+        db.commit()
 
     db = get_db()
     sale_itens = db.execute(
-        'SELECT * from sale_item WHERE sale_id = (SELECT MAX(id) FROM sale) ORDER BY id DESC'
+        'SELECT p.name, si.product_id, p.price, si.quantity, si.subtotal FROM sale_item si JOIN product p ON p.id = si.product_id WHERE si.sale_id = ?', (id,)
     ).fetchall()
 
     return render_template('pos/sale.html', sale_itens = sale_itens)
