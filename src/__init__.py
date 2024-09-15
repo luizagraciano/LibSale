515e6075124bea3ccbe24484bfccfdc962b23e21
Luiza Graciano <luizagraciano.m@gmail.com>
2024-09-15 10:45:23 -0300

    atualiza init

--- src/__init__.py
+++ src/__init__.py
@@ -6,7 +6,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'libsale.sqlite'),
     )
 
     if test_config is None:
@@ -36,5 +36,3 @@ def create_app(test_config=None):
         return render_template('home.html')
 
     return app
-
-    
